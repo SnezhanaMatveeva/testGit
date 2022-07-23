6915fdea52bb60f9c68ebcf856929d71d07d7d64
SnezhanaMatveeva <12345fam@mail.ru>
2022-07-23 13:40:50 +0300

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         SalesManager salesManager = new SalesManager(new int[]{345,78,50});
         System.out.println(salesManager.max());
     }
