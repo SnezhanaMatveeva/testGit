8eb813b0702ca1e22eb51b34a80e3e9fb897ac03
SnezhanaMatveeva <12345fam@mail.ru>
2022-07-29 12:53:15 +0300

    change int to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{345,78,50});
+        SalesManager salesManager = new SalesManager(new long[]{345,78,50});
         System.out.println(salesManager.max());
     }
 }
