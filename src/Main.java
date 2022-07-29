ac2682f13c140669bba0406aba341a42eece77e2
SnezhanaMatveeva <12345fam@mail.ru>
2022-07-29 21:27:20 +0300

    add truncatedMean

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         SalesManager salesManager = new SalesManager(new int[]{345,78,50});
         System.out.println(salesManager.max());
+        System.out.println(salesManager.truncatedMean());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -3,6 +3,7 @@ public class SalesManager {
     protected int[] sales;
 
     public SalesManager(int[] sales) {
+
         this.sales = sales;
     }
 
@@ -14,5 +15,36 @@ public class SalesManager {
             }
         }
         return max;
+    }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int truncatedMean() {
+        int sum = 0;
+        int kolvo = 0;
+        int max = max();
+        int min = min();
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] != max && sales[i] != min) {
+                sum = sum + sales[i];
+                kolvo = kolvo + 1;
+            }
+        }
+
+
+        int result=0;
+        if (kolvo != 0) {
+            result = sum / kolvo;
+        }
+        return result;
     }
 }
